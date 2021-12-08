a5a405c7b2eb04a51c73a21d2c96b128bea4c7da
Artem Melentev <tema2208st@mail.ru>
2021-12-08 20:23:19 +0700

    Second commit

--- Sapper_last_task_Donskich/src/com/company/MainForm.java
+++ Sapper_last_task_Donskich/src/com/company/MainForm.java
@@ -21,23 +21,22 @@ public class MainForm extends JFrame {
     private JButton[][] buttons;
     private JLabel[][] labels;
     private int[][] minefield;
-    private int height = 9;
-    private int width = 9;
-    private int maxmins = 10;
-    private int gameStatus1; // До начала  -2, в игре = -1, выиграно = 1, проиграно = 0
+    private int height = 16;
+    private int width = 16;
+    private int maxmins = 40;
     private int activeCells;
     private int flagsCount = maxmins;
     private int secondCount = 0;
     private Timer timer;
     private GameStatus gameStatus;
+    private Color buttonColor;
+    private Color labelColor;
 
     public MainForm() {
-        gameStatus1 = -2;
         gameStatus = GameStatus.BEFORE;
 
         setContentPane(mainPanel);
 
-        mainPanel.setBackground(Color.lightGray);
         setResizable(false);
 
         innerPanel.setLayout(new GridLayout(height, width));
@@ -46,14 +45,13 @@ public class MainForm extends JFrame {
         pack();
         innerPanel.setSize(innerPanel.getPreferredSize());
 
-        holders = new JPanel[width][height]; // holders - состоит из панелей
+        holders = new JPanel[width][height];
         buttons = new JButton[width][height];
         labels = new JLabel[width][height];
-        minefield = new int[width][height]; // массив заполненный числами от -1 до 8, где -1 - бомба. от 0 до 8 - количество бомб вокруг
+        minefield = new int[width][height];
 
-        ButtonFlagger buttonFlagger = new ButtonFlagger(); // ставит флажок на кнопку
-        ButtonOpener buttonOpener = new ButtonOpener(); // Открывает кнопку
-
+        ButtonFlagger buttonFlagger = new ButtonFlagger();
+        ButtonOpener buttonOpener = new ButtonOpener();
         flagsLabel.setText(String.format("%03d",flagsCount));
 
         for (int y = 0; y < height; ++y) {
@@ -82,36 +80,33 @@ public class MainForm extends JFrame {
 
         restartButton.addActionListener(new buttonRestarter());
 
+        buttonColor = buttons[0][0].getBackground();
+        labelColor = labels[0][0].getBackground();
+
         setLocationRelativeTo(null);
         pack();
         setVisible(true);
     }
 
-
-    private void createFiled(int[][] filed, int maxmins, int origin_x, int origin_y) { //нужно заполнять только при нажатии, и первая пустая + что бы не накладывались друг на друга
-        int k = 0;
-        while (k < maxmins){
-            // заполнили поле бомбами
-            k += 1;
-            int bomb_x = (int) (Math.random() * 9);
-            int bomb_y = (int) (Math.random() * 9);
-            if (filed[bomb_x][bomb_y] == -1) k--;
+    private void createFiled(int[][] filed, int origin_x, int origin_y) {
+        int bomb_x;
+        int bomb_y;
+        for (int k = 0; k < maxmins; k++){
+            do{
+                bomb_x = (int) (Math.random() * height);
+                bomb_y = (int) (Math.random() * width);
+            }while(filed[bomb_x][bomb_y] ==- 1 || (bomb_x==origin_x && bomb_y==origin_y));
             filed[bomb_x][bomb_y] = -1;
-            if (bomb_x == origin_x && bomb_y == origin_y) { //первая открытая клетка всегда не содержит бомбы
-                filed[origin_x][origin_y] = 0;
-                k--;
-            }
         }
         activeCells = width * height - maxmins;
-        System.out.println(activeCells);
-        for (int i = 0; i < 9; i += 1) {
-            for (int j = 0; j < 9; j += 1) {
+        for (int i = 0; i < height; i += 1) {
+            for (int j = 0; j < width; j += 1) {
                 if (filed[i][j] != -1) filed[i][j] = bombsNeighbors(filed, i, j);
             }
         }
     }
 
-    private int bombsNeighbors(int[][] filed, int i, int j) { // сделал
+    private int bombsNeighbors(int[][] filed, int i, int j) {
         int count = 0;
         int[] str_array = smartCheckForSting(i, j);
         int[] clmn_array = smartCheckForСolumns(i, j);
@@ -132,20 +127,19 @@ public class MainForm extends JFrame {
 
     private int[] smartCheckForSting(int i, int j){
         int[] array = {0, 0};
-        if (i != 0 && j != 0 && i != 8 && j != 8){
+        if (i != 0 && j != 0 && i != (height-1) && j != (width-1)){
             array[0] = -1;
             array[1] = 2;
         }
         if(i == 0){
-            //array[0] = 0;
             array[1] = 2;
         }
-        if(i == 8){
+        if(i == (height-1)){
             array[0] = -1;
             array[1] = 1;
         }
-        if(j == 0 || j == 8){
-            if(i > 0 && i < 8){
+        if(j == 0 || j == (width-1)){
+            if(i > 0 && i < (height-1)){
                 array[0] = -1;
                 array[1] = 2;
             }
@@ -155,16 +149,15 @@ public class MainForm extends JFrame {
 
     private int[] smartCheckForСolumns(int i, int j){
         int[] array = {0, 0};
-        if (i != 0 && j != 0 && i != 8 && j != 8){
+        if (i != 0 && j != 0 && i != (height - 1)  && j != (width-1)){
             array[0] = -1;
             array[1] = 2;
         }
         if(i == 0){
             if (j == 0){
-                //array[0] = 0;
                 array[1] = 2;
             }
-            else if (j == 8){
+            else if (j == (width-1)){
                 array[0] = -1;
                 array[1] = 1;
             }
@@ -173,12 +166,11 @@ public class MainForm extends JFrame {
                 array[1] = 2;
             }
         }
-        if (i == 8){
+        if (i == (height - 1)){
             if(j == 0){
-               // array[0] = 0;
                 array[1] = 2;
             }
-            else if(j == 8){
+            else if(j == (width-1)){
                 array[0] = -1;
                 array[1] = 1;
             }
@@ -189,12 +181,11 @@ public class MainForm extends JFrame {
         }
         if(j == 0){
             if(i > 0 && i < 8){
-               // array[0] = 0;
                 array[1] = 2;
             }
         }
-        if(j == 8){
-            if(i > 0 && i < 8){
+        if(j == (width-1)){
+            if(i > 0 && i < (height - 1)){
                 array[0] = -1;
                 array[1] = 1;
             }
@@ -202,25 +193,8 @@ public class MainForm extends JFrame {
         return array;
     }
 
-    private void startOpenButtons(int x, int y){
-        // нажимаем на кнопку, открывает все рядом стоящие пустые кнопки
-        int[] str_array = smartCheckForSting(x, y);
-        int[] clmn_array = smartCheckForСolumns(x, y);
-        int str_bot = str_array[0];
-        int str_top = str_array[1];
-        int clmn_bot = clmn_array[0];
-        int clmn_top = clmn_array[1];
-        for (int i = str_bot; i < str_top; i ++) {
-            for (int j = clmn_bot; j < clmn_top; j++) {
-                if (minefield[x + i][y + j] != -1) {
-                    openEmptyButton(x + i, y + j);
-                }
-            }
-        }
-    }
-
     private void openEmptyButton(int x, int y) {
-        if(minefield[x][y] == 0 && buttons[x][y].getParent()!=null){ // если нет поблизости мин и мы эту кнопку не открывали
+        if(minefield[x][y] == 0 && buttons[x][y].getParent()!=null){
             open(x,y);
             int[] str_array = smartCheckForSting(x, y);
             int[] clmn_array = smartCheckForСolumns(x, y);
@@ -231,7 +205,7 @@ public class MainForm extends JFrame {
 
             for (int i = str_bot; i < str_top; i++) {
                 for (int j = clmn_bot; j < clmn_top; j++) {
-                    if (i != 0 || j != 0) openEmptyButton(x + i, y + j);
+                    if ((i != 0 || j != 0) && !buttons[x + i][y + j].getText().equals("!")) openEmptyButton(x + i, y + j);
                 }
             }
         }
@@ -241,15 +215,18 @@ public class MainForm extends JFrame {
     private void open(int x, int y){
         labels[x][y].setText(Integer.toString(minefield[x][y]));
         if (minefield[x][y] == 0) labels[x][y].setText("");
-        if (minefield[x][y] == -1) labels[x][y].setText("*");
-        if (minefield[x][y] == -1 && gameStatus1 == -1) { // если наступили на мину - проигрыш
-            gameStatus1 = 0;
+        if (minefield[x][y] == -1){
+            labels[x][y].setText("*");
+            labels[x][y].setForeground(Color.BLACK);
+        }
+        if (minefield[x][y] == -1 && gameStatus == GameStatus.DURING) {
+            gameStatus = GameStatus.LOSE;
             labels[x][y].setBackground(Color.RED);
             labels[x][y].setOpaque(true);
         }
         holders[x][y].remove(buttons[x][y]);
         holders[x][y].add(labels[x][y]);
-        if (gameStatus1 == -1 || gameStatus1 == -2) activeCells -= 1;
+        if ((gameStatus == GameStatus.DURING || gameStatus == GameStatus.BEFORE) && minefield[x][y] != -1) activeCells -= 1;
 
         switch (minefield[x][y])
         {
@@ -265,15 +242,12 @@ public class MainForm extends JFrame {
     }
 
     private void checkGameStatus(){
-        switch (gameStatus1){
-            case -2:
-                System.out.println("The game has not started yet");
-                break;
-            case 0:
+        switch (gameStatus){
+            case LOSE:
                 lose();
                 timer.cancel();
                 break;
-            case 1:
+            case WIN:
                 win();
                 timer.cancel();
                 break;
@@ -281,24 +255,21 @@ public class MainForm extends JFrame {
     }
 
     private class ButtonFlagger extends MouseAdapter {
-
         public void mousePressed(MouseEvent e) {
             JPanel p = (JPanel) (((JButton) (e.getSource())).getParent());
             int Cx = p.getX() / 40, Cy = p.getY() / 40;
-            if ((gameStatus1 == -2 || gameStatus1 == -1) && e.getButton() == 3) { // если нажали на парвую кнопку мыши
-                if (Objects.equals(buttons[Cx][Cy].getText(), "!")) {
-                    buttons[Cx][Cy].setText(""); // если захотим убрать флажок
-                    flagsCount--;
+            if ((gameStatus == GameStatus.BEFORE || gameStatus == GameStatus.DURING) && e.getButton() == 3) {
+                if (buttons[Cx][Cy].getText().equals("!")) {
+                    buttons[Cx][Cy].setText("");
+                    flagsCount++;
                 }
                 else{
                     buttons[Cx][Cy].setText("!");
                     flagsCount--;
                 }
                 flagsLabel.setText(String.format("%03d",flagsCount));
-                innerPanel.revalidate();
-                innerPanel.repaint();
             }
-            checkGameStatus(); // что бы выводило что нельзя ставить флажки после завершения игры
+            checkGameStatus();
         }
     }
 
@@ -307,16 +278,16 @@ public class MainForm extends JFrame {
         public void actionPerformed(ActionEvent e) {
             JPanel p = (JPanel) (((JButton) (e.getSource())).getParent());
             int Cx = p.getX() / 40, Cy = p.getY() / 40;
-            if(gameStatus1 == -2 && !Objects.equals(buttons[Cx][Cy].getText(), "!")){
-                createFiled(minefield, maxmins, Cx, Cy);
-                startOpenButtons(Cx, Cy);
+            if(gameStatus == GameStatus.BEFORE && !buttons[Cx][Cy].getText().equals("!")){
+                createFiled(minefield, Cx, Cy);
+                openEmptyButton(Cx, Cy);
                 timer = new Timer();
                 timer.schedule(new MyTimer(), 0, 1000);
-                gameStatus1 = -1; // меняем статус игры на "В игре"
+                gameStatus = GameStatus.DURING;
             }
-            if (!Objects.equals(buttons[Cx][Cy].getText(), "!") && gameStatus1 == -1) {
+            if (!buttons[Cx][Cy].getText().equals("!") && gameStatus == GameStatus.DURING) {
                 openEmptyButton(Cx, Cy);
-                if (activeCells == 0) gameStatus1 = 1;
+                if (activeCells == 0) gameStatus = GameStatus.WIN;
             }
             checkGameStatus();
             innerPanel.revalidate();
@@ -334,17 +305,18 @@ public class MainForm extends JFrame {
     }
 
     private class buttonRestarter implements ActionListener {
-        //игровое поле должно заполниться нулями, нужно создать новый таймер,
         @Override
         public void actionPerformed(ActionEvent e) {
-            setNulls(); // Зануляем
-            gameStatus1 = -2; // переводим в режим "до начала"
+            setNulls();
+            gameStatus = GameStatus.BEFORE;
             timer.cancel();
             timer = new Timer();
             secondCount = 0;
             timerLabel.setText(String.format("%03d", secondCount));
             for (int x = 0; x < height; x++) {
-                for (int y = 0; y < height; y++) {
+                for (int y = 0; y < width; y++) {
+                    labels[x][y].setBackground(labelColor);
+                    buttons[x][y].setBackground(buttonColor);
                     if (buttons[x][y].getParent() == null) {
                         holders[x][y].remove(labels[x][y]);
                         holders[x][y].add(buttons[x][y]);
@@ -353,6 +325,7 @@ public class MainForm extends JFrame {
                 }
             }
             flagsCount = maxmins;
+            flagsLabel.setText(String.format("%03d",flagsCount));
             innerPanel.revalidate();
             innerPanel.repaint();
         }
@@ -360,7 +333,7 @@ public class MainForm extends JFrame {
 
     private void setNulls(){
         for (int i = 0; i < height; i++){
-            for (int j = 0; j < height; j++){
+            for (int j = 0; j < width; j++){
                 minefield[i][j] = 0;
             }
         }
@@ -368,7 +341,7 @@ public class MainForm extends JFrame {
 
     private void win(){
         for (int i = 0; i < height; i++){
-            for (int j = 0; j < height; j++){
+            for (int j = 0; j < width; j++){
                 if (minefield[i][j] == -1) buttons[i][j].setText("!");
             }
         }
@@ -376,9 +349,9 @@ public class MainForm extends JFrame {
 
     private void lose(){
         for (int i = 0; i < height; i++){
-            for (int j = 0; j < height; j++){
-                if (minefield[i][j] == -1 && !Objects.equals(buttons[i][j].getText(), "!")) open(i, j);
-                if (minefield[i][j] == -1 && Objects.equals(buttons[i][j].getText(), "!")){
+            for (int j = 0; j < width; j++){
+                if (minefield[i][j] == -1 && !buttons[i][j].getText().equals("!")) open(i, j);
+                if (minefield[i][j] != -1 && buttons[i][j].getText().equals("!")){
                     buttons[i][j].setBackground(Color.YELLOW);
                     buttons[i][j].setOpaque(true);
                 }
